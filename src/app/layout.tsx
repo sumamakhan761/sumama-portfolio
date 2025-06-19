e350d7285c7dc5848a2a6d74971b1f35c58d6200
sumamakhan7611 <khansumama87111@gmail.com>
2025-06-19 05:48:48 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 import { siteConfig } from "@/lib/config";
 
@@ -84,6 +85,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
